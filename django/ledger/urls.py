fd64cf1c1f711a23bb8005044fd04f9ac12562b6
Borland <jborland@tecnova.com>
2020-08-28 14:23:27 -0500

    added catch-all url to django to catch react router urls for now

--- django/ledger/urls.py
+++ django/ledger/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-	path('django_entities/', views.django_entities, name='django_entities'),
-	path('django_gettransaction/', views.django_gettransaction, name='django_gettransaction'),
-	path('django_getuser/', views.django_getuser, name='django_getuser'),
-	path('django_ledger/', views.django_ledger, name='django_ledger'),
-	path('django_login/', views.django_login, name='django_login'),
-	path('django_logout/', views.django_logout, name='django_logout'),
-	path('django_register/', views.django_register, name='django_register'),
-	path('django_transactiontypes/', views.django_transactiontypes, name='django_transactiontypes')
+    path('django_entities/', views.django_entities, name='django_entities'),
+    path('django_gettransaction/', views.django_gettransaction, name='django_gettransaction'),
+    path('django_getuser/', views.django_getuser, name='django_getuser'),
+    path('django_ledger/', views.django_ledger, name='django_ledger'),
+    path('django_login/', views.django_login, name='django_login'),
+    path('django_logout/', views.django_logout, name='django_logout'),
+    path('django_register/', views.django_register, name='django_register'),
+    path('django_transactiontypes/', views.django_transactiontypes, name='django_transactiontypes'),
+    re_path(r'^(?:.*)/?$', views.index, name='index')
 ]
